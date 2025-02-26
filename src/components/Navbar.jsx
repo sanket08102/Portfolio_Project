a342257227eb7b63a66cfa327fcd1a4958f3f591
Sanket Teli <telisanket8@gmail.com>
2025-02-26 12:49:36 +0530

    Title Changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            <span className='sm:block hidden'>Code With Sanket</span>
+            <span className='sm:block hidden'>I Am Sanket</span>
           </p>
         </Link>
 
